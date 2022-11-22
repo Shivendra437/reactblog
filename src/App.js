43fead57ac81e16dc34a6a8cde27e245f5f84368
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2022-11-22 20:37:42 +0530

    update router

--- src/App.js
+++ src/App.js
@@ -5,32 +5,24 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const currentUser = true;
+  const user = false;
   return (
     <Router>
       <Topbar />
-      <Switch>
-        <Route exact path="/">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        {/* <Route path="/posts">
           <Homepage />
-        </Route>
-        <Route path="/posts">
-          <Homepage />
-        </Route>
-        <Route path="/register">
-          {currentUser ? <Homepage /> : <Register />}
-        </Route>
-        <Route path="/login">{currentUser ? <Homepage /> : <Login />}</Route>
-        <Route path="/post/:id">
-          <Single />
-        </Route>
-        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
-        <Route path="/settings">
-          {currentUser ? <Settings /> : <Login />}
-        </Route>
-      </Switch>
+        </Route> */}
+        <Route path="/register" element={user ? <Homepage /> : <Register />} />
+        <Route path="/login" element={user ? <Homepage /> : <Login />} />
+        <Route path="/post/:id" element={<Single />} />
+        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Login />} />
+      </Routes>
     </Router>
   );
 }
